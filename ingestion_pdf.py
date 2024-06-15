f95b0aa7919e099c81aca52919df897fbbb496b7
kevinArgueta96 <41269695+kevinArgueta96@users.noreply.github.com>
2024-06-15 17:06:25 -0600

    Use templates to create answers an ingestion PDF files into pinecone

--- ingestion_pdf.py
+++ ingestion_pdf.py
@@ -1,13 +1,5 @@
 from dotenv import load_dotenv
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import FAISS
-from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_community.llms import Ollama
-from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_openai import OpenAIEmbeddings
-from sentence_transformers import SentenceTransformer
 
 from consts import INDEX_NAME
 from langchain_community.vectorstores import Pinecone as PineconeLangChain
@@ -15,13 +7,10 @@ from pinecone import Pinecone
 
 
 ## NEW UPLOAD PINECONE LIBRARIES
-from langchain_community.document_loaders import TextLoader
 from langchain_openai import OpenAIEmbeddings
-from langchain_text_splitters import CharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
 
 load_dotenv()
-
-from PyPDF2 import PdfReader
 
 import os
 
@@ -29,34 +18,27 @@ pc = Pinecone(
     api_key=os.environ.get("PINECONE_API_KEY"),
 )
 
-# load PDF file
-pdf_file = open("pdf-files/codigo-de-trabajo.pdf", "rb")
-pdf_reader = PdfReader(pdf_file)
+# load PDF file using loaders from langchain
+loader = PyPDFLoader("pdf-files/codigo-de-trabajo.pdf")
+document = loader.load_and_split()
 
-text = ""
-# Use the .pages attribute to get a list of page objects
-for page in pdf_reader.pages:
-    page_text = page.extract_text()
-    if page_text:  # Check if there is text to add
-        text += page_text
-
-pdf_file.close()
-
-loader = TextLoader(text, encoding='utf-8')
-documents = loader.load()
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=800,
     chunk_overlap=100,
     length_function=len
 )
-docs = text_splitter.split_documents(documents)
 
-chunks = text_splitter.split_text(text)
-
-#embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/paraphrase-multilingual-mpnet-base-v2")
+docs = text_splitter.split_documents(document)
 embeddings = OpenAIEmbeddings()
 
-PineconeLangChain.from_documents(documents, embeddings, index_name=INDEX_NAME)
+print("****Loading to vectorstore***")
+PineconeLangChain.from_documents(docs, embeddings, index_name=INDEX_NAME)
+print("****Loading to vectorstore***")
+
+#embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/paraphrase-multilingual-mpnet-base-v2")
+#embeddings = OpenAIEmbeddings()
+
+#PineconeLangChain.from_documents(documents, embeddings, index_name=INDEX_NAME)
 
 #print("PART OF CHUNK:" + chunks[7])
 #print(len(chunks))
@@ -68,7 +50,7 @@ PineconeLangChain.from_documents(documents, embeddings, index_name=INDEX_NAME)
 
 #knowledge_base = FAISS.from_texts(chunks, embeddings)
 
-question = "¿Cuáles son las obligaciones específicas que debe cumplir el agente reclutador o la empresa, según el artículo 34, para garantizar la protección y bienestar de los trabajadores guatemaltecos que son contratados para trabajar fuera del país?"
+#question = "¿Cuáles son las obligaciones específicas que debe cumplir el agente reclutador o la empresa, según el artículo 34, para garantizar la protección y bienestar de los trabajadores guatemaltecos que son contratados para trabajar fuera del país?"
 #docs = knowledge_base.similarity_search(question, 3)
 
 #PineconeLangChain.from_documents(text, embeddings, index_name=INDEX_NAME)
---
+++ question_pdf.py
@@ -0,0 +1,88 @@
+import os
+
+from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+
+from langchain import hub
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain_pinecone import PineconeVectorStore
+from consts import INDEX_NAME
+from pinecone import Pinecone
+
+# USE LOCAL LLM
+from langchain_community.llms import Ollama
+
+load_dotenv()
+
+
+def format_docs(docs):
+    print(docs)
+    return "\n\n".join(docs.page_content for doc in docs)
+
+
+if __name__ == "__main__":
+    print("Welcome to question to codigo de trabajo")
+
+    pc = Pinecone(
+        api_key=os.environ.get("PINECONE_API_KEY"),
+    )
+
+    #query = "¿Qué estipula el artículo 13 sobre la contratación de trabajadores guatemaltecos en comparación con trabajadores extranjeros?"
+    #query = "¿Cuales son las horas que un trabajador debe tener y que jornadas existen?, si puedes extenderte, hazlo"
+    query = "¿Cuales son las horas que un trabajador debe tener y que jornadas existen?, si puedes extenderte, hazlo"
+
+    embeddings = OpenAIEmbeddings()
+    llm = Ollama(model="mistral")
+    # llm = ChatOpenAI()
+
+    vectorstore = PineconeVectorStore(embedding=embeddings, index_name=INDEX_NAME)
+
+    """"
+    // We obtain the prompt template from here:
+    https://smith.langchain.com/hub/langchain-ai/retrieval-qa-chat
+    langchain-ai/retrieval-qa-chat
+    SYSTEM
+    Answer any use questions based solely on the context below:
+    <context>
+    {context}
+    </context>
+    PLACEHOLDER
+    chat_history
+    HUMAN
+    {input}
+    """
+    retrieval_qa_chain_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+    combine_docs_chain = create_stuff_documents_chain(llm, retrieval_qa_chain_prompt)
+    retrieval_chain = create_retrieval_chain(
+        retriever=vectorstore.as_retriever(), combine_docs_chain=combine_docs_chain
+    )
+
+    template = """Use the following pieces of context to answer the question at the end.
+
+    If you don't know the answer, just say that you don't know, don't try to make up an answer
+    Use three sentences maximum and keep the answer as concise as possible.
+
+    Always say "Gracias por preguntar!" at the end of the answer.
+    Always response in spanish language
+    
+    If you have the source of the answer, said (Obtenida de: "SET HER THE URL OR SOURCE))
+    {context}
+
+    Question: {question}
+
+    Helpful Answer:"""
+    custom_rag_prompt = ChatPromptTemplate.from_template(template)
+
+    #print(vectorstore.as_retriever())
+    rag_chain = (
+            {"context": vectorstore.as_retriever(), "question": RunnablePassthrough()}
+            | custom_rag_prompt
+            | llm
+    )
+
+    # result = retrieval_chain.invoke(input={"input": query})
+    res = rag_chain.invoke(query)
+    print(res)
